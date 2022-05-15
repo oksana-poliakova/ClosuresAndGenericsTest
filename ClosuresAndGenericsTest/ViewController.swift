3cf4cedfcb8663d580103f6fc9a9d4ccbe955c1d
Oksana Poliakova <it.oksana.poliakova@gmail.com>
2022-05-15 12:54:08 +0300

    Initial Commit

---
+++ ClosuresAndGenericsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClosuresAndGenericsTest
+//
+//  Created by Oksana Poliakova on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
